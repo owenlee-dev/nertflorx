a88dffe7be24ccd35fbb224989398512fd6b5541
owenlee-dev <70532700+owenlee-dev@users.noreply.github.com>
2020-11-28 20:21:01 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+API STUFF
+
+KEY: 'e5d5472bade839492cd6cd426951372d'
+
+Example API Request : https://api.themoviedb.org/3/movie/550?api_key=e5d5472bade839492cd6cd426951372d
+
+featured: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e5d5472bade839492cd6cd426951372d&page=1"
+
+API Read Access Token
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWQ1NDcyYmFkZTgzOTQ5MmNkNmNkNDI2OTUxMzcyZCIsInN1YiI6IjVmNzc3ZjBkN2I3YjRkMDAzOTZkYThlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Bo-hcdHNdoxjdM3Y1Vij4uiYKe3IelwSXV6TnDnyVA
+
+//TOD
+pagination
+search solarmovies button
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+import Movie from "./components/Movie";
+import {
+  getFeatured,
+  getGenre,
+  getSearched,
+  setCurrentPage,
+} from "./components/AppSlice";
+import { useSelector, useDispatch } from "react-redux";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
+//make featured page refresh with randomly selected movies
+//main application
+function App() {
+  const dispatch = useDispatch();
+  const { movieData, selectedGenre, userInput, currentPage } = useSelector(
+    (state) => state.app
+  );
+
+  //on page load, get featured titles
+  useEffect(() => {
+    dispatch(getFeatured(1));
+  }, []);
+
+  //if going through search results, next page of search
+  //if going through selected genre, next page of genre
+  //if not then next page of featured
+  const nextPageHandler = () => {
+    if (selectedGenre) {
+      const toSend = {
+        genreId: selectedGenre,
+        pageNumber: currentPage + 1,
+      };
+      dispatch(getGenre(toSend));
+    } else if (userInput) {
+      const toSend = { userInput: userInput, pageNumber: currentPage + 1 };
+      dispatch(getSearched(toSend));
+    } else {
+      dispatch(getFeatured(currentPage + 1));
+    }
+    dispatch(setCurrentPage(currentPage + 1));
+    window.scrollTo(0, 0);
+  };
+  const prevPageHandler = () => {
+    if (selectedGenre) {
+      const toSend = {
+        genreId: selectedGenre,
+        pageNumber: currentPage - 1,
+      };
+      dispatch(getGenre(toSend));
+    } else if (userInput) {
+      const toSend = { userInput: userInput, pageNumber: currentPage - 1 };
+      dispatch(getSearched(toSend));
+    } else {
+      dispatch(getFeatured(currentPage - 1));
+    }
+    dispatch(setCurrentPage(currentPage - 1));
+    console.log(currentPage);
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar />
+      <Header />
+      <div className="content-wrapper">
+        {movieData.length > 0 &&
+          movieData.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+      <div className="page-nav-btns">
+        {currentPage > 1 && (
+          <button className="page-nav-btn" onClick={prevPageHandler}>
+            previous page
+          </button>
+        )}
+        <button className="page-nav-btn" onClick={nextPageHandler}>
+          next page
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
